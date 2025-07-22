33fcce065b8fd9f01a45cf57d0e226f39f08ea2e
Edrich Chua <edrichhans@gmail.com>
2025-07-22 21:43:43 +0800

    feat: config

---
+++ cmd/config.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"sync"
+
+	"github.com/caarlos0/env/v11"
+)
+
+type Config struct {
+	ServerPort     int    `env:"SERVER_PORT" envDefault:"8080"`
+	BinanceAddress string `env:"BINANCE_ADDRESS" envDefault:"stream.binance.com"`
+	BinancePort    int    `env:"BINANCE_PORT" envDefault:"9443"`
+}
+
+var (
+	instance *Config
+	once     sync.Once
+)
+
+func (c *Config) Load() error {
+	var cfg Config
+	if err := env.Parse(&cfg); err != nil {
+		return err
+	}
+	*c = cfg
+	return nil
+}
+
+func GetConfig() *Config {
+	once.Do(func() {
+		instance = &Config{}
+		instance.Load()
+	})
+	return instance
+}
