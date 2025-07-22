3cc6dcf41c8c1e858e2096e596ec4002d7686eba
Edrich Chua <edrichhans@gmail.com>
2025-07-22 21:44:02 +0800

    refactor: separate candles server

---
+++ internal/candles/service.go
@@ -0,0 +1,93 @@
+package candles
+
+import (
+	"context"
+	candlesv1 "hermeneutic-candles/gen/proto/candles/v1"
+	"hermeneutic-candles/internal/exchange"
+	"hermeneutic-candles/internal/exchange/binance"
+	"log"
+	"time"
+
+	"connectrpc.com/connect"
+)
+
+type CandlesService struct{}
+
+func (s *CandlesService) StreamCandles(
+	ctx context.Context,
+	req *connect.Request[candlesv1.StreamCandlesRequest],
+	serverstream *connect.ServerStream[candlesv1.StreamCandlesResponse],
+) error {
+	ticker := time.NewTicker(time.Duration(req.Msg.Interval) * time.Second)
+	defer ticker.Stop()
+
+	// TODO:
+	// Get the symbol and interval from the request
+	binanceAdapter := &binance.BinanceAdapter{}
+	tradeChannel := make(chan exchange.Trade)
+
+	go func() {
+		if err := binanceAdapter.StreamTrades(ctx, []string{"btcusdt"}, tradeChannel); err != nil {
+			log.Printf("failed to stream trades: %v", err)
+		}
+	}()
+
+	go func() {
+		trades := []exchange.Trade{}
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case trade := <-tradeChannel:
+				trades = append(trades, trade)
+			case <-ticker.C:
+				if len(trades) == 0 {
+					continue
+				}
+				candle := s.tradesToCandle(trades)
+				if err := serverstream.Send(candle); err != nil {
+					log.Printf("failed to send candle: %v", err)
+					return
+				}
+				// Reset trades for the next interval
+				trades = trades[:0]
+			}
+		}
+
+	}()
+
+	<-ctx.Done()
+
+	return nil
+}
+
+func (s *CandlesService) tradesToCandle(trades []exchange.Trade) *candlesv1.StreamCandlesResponse {
+	if len(trades) == 0 {
+		return nil
+	}
+
+	open := trades[0].Price
+	high := open
+	low := open
+	close := trades[len(trades)-1].Price
+	volume := 0.0
+
+	for _, trade := range trades {
+		if trade.Price > high {
+			high = trade.Price
+		}
+		if trade.Price < low {
+			low = trade.Price
+		}
+		volume += trade.Quantity
+	}
+
+	return &candlesv1.StreamCandlesResponse{
+		Timestamp: trades[0].Timestamp.Unix(),
+		Open:      open,
+		High:      high,
+		Low:       low,
+		Close:     close,
+		Volume:    volume,
+	}
+}
